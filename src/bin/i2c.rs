b9ce48fd256b0dec13403ff9ff9ad469bbbaa7a3
Henrik Alsér <henrik.alser@me.com>
2021-03-03 08:57:16 +0100

    Cleanup

--- src/bin/i2c.rs
+++ src/bin/i2c.rs
@@ -19,6 +19,8 @@ mod app {
     };
 
     const ADDR: u8 = 0x34 >> 1;
+    const ID_REG: u32 = 0x0000;
+    const VOL_REG: u16 = 0x1C;
 
     #[resources]
     struct Resources {
@@ -63,7 +65,7 @@ mod app {
             // Get Device ID
             let mut buf = [0u8; 2];
             ctx.resources.i2c.lock(|i2c| {
-                i2c.write_read(ADDR, &0x00u32.to_be_bytes(), &mut buf).ok();
+                i2c.write_read(ADDR, &ID_REG.to_be_bytes(), &mut buf).ok();
             });
             defmt::info!("Device ID: {:?}", u16::from_be_bytes(buf));
             cortex_m::asm::delay(216_000_000);
@@ -71,16 +73,14 @@ mod app {
             // Get Vol
             let mut buf = [0u8; 2];
             ctx.resources.i2c.lock(|i2c| {
-                i2c.write_read(ADDR, &(0x1Cu16).to_be_bytes(), &mut buf)
-                    .ok();
+                i2c.write_read(ADDR, &VOL_REG.to_be_bytes(), &mut buf).ok();
             });
             defmt::info!("Vol: {:?}", u16::from_be_bytes(buf));
 
             // Set Vol
             let mut buf = [0u8; 4];
-            const REG: u16 = 0x1C;
             let data = 0x11u16;
-            buf[..2].copy_from_slice(&REG.to_be_bytes());
+            buf[..2].copy_from_slice(&VOL_REG.to_be_bytes());
             buf[2..].copy_from_slice(&data.to_be_bytes());
             ctx.resources.i2c.lock(|i2c| {
                 i2c.write(ADDR, &buf).ok();
@@ -90,8 +90,7 @@ mod app {
             // Get Vol
             let mut buf = [0u8; 2];
             ctx.resources.i2c.lock(|i2c| {
-                i2c.write_read(ADDR, &(0x1Cu16).to_be_bytes(), &mut buf)
-                    .ok();
+                i2c.write_read(ADDR, &VOL_REG.to_be_bytes(), &mut buf).ok();
             });
             defmt::info!("Vol: {:?}", u16::from_be_bytes(buf));
             cortex_m::asm::delay(216_000_000);
